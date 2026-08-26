95a6e853f113b01298e9565ddbf893c9b84c91ff
bmgneil <yiuling2@protonmail.com>
2026-08-26 03:05:18 -0400

    aaa

--- server.js
+++ server.js
@@ -4,32 +4,32 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const allowedOrigin = process.env.ALLOWED_ORIGIN;
 
-app.use(express.json());
-
 app.use((req, res, next) => {
-  if (req.headers.origin === allowedOrigin) {
-    res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-  }
+    if (req.headers.origin === allowedOrigin) {
+        res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+    }
 
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
 
-  next();
+    next();
 });
 
+app.use(express.json());
+
 app.post("/api/notify", (req, res) => {
-  console.log("POST received:", req.body);
-  res.json({ status: "success" });
+    console.log("POST received:", req.body);
+    res.json({ status: "success", message: "Webhook received" });
 });
 
 app.get("/", (req, res) => {
-  res.send("Webhook server is running");
+    res.send("Webhook server is running");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
